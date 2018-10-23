f34dbd55f057f809d8e901807895532b7b0576b6
root <root@ip-172-31-6-113.ec2.internal>
2018-10-23 13:24:12 +0000

    automated backup

--- jvandiver/scripts-backup/backup.sh
+++ jvandiver/scripts-backup/backup.sh
@@ -1,6 +1,6 @@
 cd /home
 git pull
-find /home -type f -not -path "*/.*/*" -not -name ".*" | while read filename
+find /home -not -path "*/.*/*" -not -name ".*" | while read filename
 do
 	owner=$(echo ${filename#/home/} | cut -f1 -d"/")
 	echo $owner
