37b5adb06de996a50a391435e358c338005b2a96
root <root@ip-172-31-50-214.ec2.internal>
2019-03-10 20:15:03 +0000

    automated backup

--- apatel/script.py
+++ apatel/script.py
@@ -6,10 +6,18 @@ response = client.list_environments()
 
 print response
 
+# response = client.create_environment_membership(
+#         environmentId='41ac563ec1af481e8b7dae28920b0352',
+#         userArn='arn:aws:iam::150404946404:root',
+#         permissions='read-write'
+#     )
+    
+# print response
+
 for environment in response['environmentIds']:
     response = client.create_environment_membership(
         environmentId=environment,
-        userArn='arn:aws:iam::150404946404:root',
+        userArn='arn:aws:iam::150404946404:user/jvandiver1',
         permissions='read-write'
     )
 
