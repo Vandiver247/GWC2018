8b6e269d69c3de612a660fd6eda9368b186326a5
root <root@ip-172-31-81-205.ec2.internal>
2019-03-11 22:50:04 +0000

    automated backup

--- apatel/script.py
+++ apatel/script.py
@@ -1,6 +1,6 @@
 import boto3
 
-users = ["apatel1","araval1","amkleinle1","ankleinle1","agupta1","awang1","djain1","edouglas1","ekrever1","ireyes1","iqi1","jauslander1","kli1","mtakahashi1","nmadhav1","nbhatia1","pmurali1","rpatnaik1","jvandiver1","rnagarakanti1","srao1","sshekhawat1","sghatak1","slee1","vagarwal1","smurthy1","usharma1","bburkhardt1","mkauffman1","bkowalczyk1","mvyas1","tnamjoshi1"]
+users = ["apatel1","araval1","amkleinle1","ankleinle1","agupta1","awang1","djain1","edouglas1","ekrever1","ireyes1","iqi1","jauslander1","kli1","mtakahashi1","nmadhav1","nbhatia1","pmurali1","rpatnaik1","rnagarakanti1","srao1","sshekhawat1","sghatak1","slee1","vagarwal1","smurthy1","usharma1","bburkhardt1","mkauffman1","bkowalczyk1","mvyas1","tnamjoshi1"]
 
 client = boto3.client('cloud9')
 
@@ -8,10 +8,10 @@ response = client.list_environments()
 
 for user in users:
     for environment in response['environmentIds']:
-        response2 = client.create_environment_membership(
+        response2 = client.delete_environment_membership(
             environmentId=environment,
             userArn="arn:aws:iam::150404946404:user/" + user,
-            permissions='read-write'
+            #permissions='read-write'
         )
     
         print response2
@@ -22,10 +22,14 @@ response = client.list_environments(
 
 for user in users:
     for environment in response['environmentIds']:
-        response2 = client.create_environment_membership(
-            environmentId=environment,
-            userArn="arn:aws:iam::150404946404:user/" + user,
-            permissions='read-write'
-        )
+        try:
+            response2 = client.delete_environment_membership(
+                environmentId=environment,
+                userArn="arn:aws:iam::150404946404:user/" + user,
+                #permissions='read-write'
+            )
+        except:
+            print "Exception"
+            continue
     
         print response2
