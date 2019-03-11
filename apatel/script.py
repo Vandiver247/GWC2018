866aa0ea647824667bfb5e2b32c9476f8287ab79
root <root@ip-172-31-81-205.ec2.internal>
2019-03-11 22:35:04 +0000

    automated backup

--- apatel/script.py
+++ apatel/script.py
@@ -1,28 +1,31 @@
 import boto3
 
+users = ["apatel1","araval1","amkleinle1","ankleinle1","agupta1","awang1","djain1","edouglas1","ekrever1","ireyes1","iqi1","jauslander1","kli1","mtakahashi1","nmadhav1","nbhatia1","pmurali1","rpatnaik1","jvandiver1","rnagarakanti1","srao1","sshekhawat1","sghatak1","slee1","vagarwal1","smurthy1","usharma1","bburkhardt1","mkauffman1","bkowalczyk1","mvyas1","tnamjoshi1"]
+
 client = boto3.client('cloud9')
 
 response = client.list_environments()
+
+for user in users:
+    for environment in response['environmentIds']:
+        response = client.create_environment_membership(
+            environmentId=environment,
+            userArn="arn:aws:iam::150404946404:user/" + user,
+            permissions='read-write'
+        )
+    
+        print response
 
 response = client.list_environments(
     nextToken=response['nextToken']
 )
 
-print len(response['environmentIds'])
-
-# response = client.create_environment_membership(
-#         environmentId='41ac563ec1af481e8b7dae28920b0352',
-#         userArn='arn:aws:iam::150404946404:root',
-#         permissions='read-write'
-#     )
+for user in users:
+    for environment in response['environmentIds']:
+        response = client.create_environment_membership(
+            environmentId=environment,
+            userArn="arn:aws:iam::150404946404:user/" + user,
+            permissions='read-write'
+        )
     
-# print response
-
-for environment in response['environmentIds']:
-    response = client.create_environment_membership(
-        environmentId=environment,
-        userArn='arn:aws:iam::150404946404:user/jvandiver1',
-        permissions='read-write'
-    )
-
-    print response
+        print response
