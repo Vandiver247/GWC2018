6a68e03a35dce33cd03577b6cd20a0d9507af21f
root <root@ip-172-31-81-205.ec2.internal>
2019-03-11 23:05:04 +0000

    automated backup

--- apatel/script.py
+++ apatel/script.py
@@ -6,30 +6,42 @@ client = boto3.client('cloud9')
 
 response = client.list_environments()
 
-for user in users:
-    for environment in response['environmentIds']:
-        response2 = client.delete_environment_membership(
-            environmentId=environment,
-            userArn="arn:aws:iam::150404946404:user/" + user,
-            #permissions='read-write'
-        )
-    
-        print response2
+for environment in response['environmentIds']:
+    env_data = client.describe_environment_memberships(environmentId=environment)
+    if len(env_data['memberships']) >= 8:
+        env_data = client.describe_environments(environmentIds=[environment])
+        print env_data['environments'][0]['name']
 
-response = client.list_environments(
-    nextToken=response['nextToken']
-)
 
-for user in users:
-    for environment in response['environmentIds']:
-        try:
-            response2 = client.delete_environment_membership(
-                environmentId=environment,
-                userArn="arn:aws:iam::150404946404:user/" + user,
-                #permissions='read-write'
-            )
-        except:
-            print "Exception"
-            continue
+
+
+
+
+
+# for user in users:
+#     for environment in response['environmentIds']:
+#         response2 = client.delete_environment_membership(
+#             environmentId=environment,
+#             userArn="arn:aws:iam::150404946404:user/" + user,
+#             #permissions='read-write'
+#         )
     
-        print response2
+#         print response2
+
+# response = client.list_environments(
+#     nextToken=response['nextToken']
+# )
+
+# for user in users:
+#     for environment in response['environmentIds']:
+#         try:
+#             response2 = client.delete_environment_membership(
+#                 environmentId=environment,
+#                 userArn="arn:aws:iam::150404946404:user/" + user,
+#                 #permissions='read-write'
+#             )
+#         except:
+#             print "Exception"
+#             continue
+    
+#         print response2
